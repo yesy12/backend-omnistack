569366c8f4d7f5fe50259a19d6bdd0f469d9c7c6
Alison <alisonvieira.av29@gmail.com>
2019-10-02 20:16:00 -0300

    inserindo link

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,6 +2,18 @@ const express = require("express");
 const app = express();
 const port = 3000;
 const routes = require("./routes");
+const mongoose = require("mongoose");
+
+
+const username = "omnistack";
+const password = "lPBVFfPw8sK4QbEC";
+const banco = "omnistack";
+const link = `mongodb+srv://${username}:${password}@cluster0-nth3w.mongodb.net/${banco}?retryWrites=true&w=majority`
+mongoose.connect(link,{ 
+    useNewUrlParser: true,
+    useUnifiedTopology: true 
+})
+
 
 app.use(express.json());
 app.use(routes);
@@ -9,8 +21,3 @@ app.use(routes);
 app.listen(port,()=>{
     console.log(`Running on port ${port} ...`)
 })
-
-const username = "omnistack";
-const password = "lPBVFfPw8sK4QbEC";
-const banco = "omnistack";
-const link = `mongodb+srv://${username}:${password}@cluster0-nth3w.mongodb.net/${banco}?retryWrites=true&w=majority`
