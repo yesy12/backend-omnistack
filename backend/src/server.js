95f58e985d3c4eefc086ac53f5b9cc25f3d14a1d
Alison <alisonvieira.av29@gmail.com>
2019-10-02 20:02:43 -0300

    tirando rotas de index.js

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,26 +2,6 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
-//req.query para acessar os parametros ?idade=20
-//para filtros
-
-//req.params acessar parametros da url users/1
-//para edicao,delete
-
-//req.body Acessar o corpo da requisição
-
-app.use(express.json())
-
-app.post("/users",(req,res)=>{
-    return res.json(req.body)
-})
-
-app.put("/users/:id",(req,res)=>{
-    return res.json({
-        id : req.params.id
-    })
-})
-
 app.listen(port,()=>{
     console.log(`Running on port ${port} ...`)
 })
