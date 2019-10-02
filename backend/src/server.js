84ea25e59374fe8fd37bb452989b5578d71661b7
Alison <alisonvieira.av29@gmail.com>
2019-10-02 19:24:18 -0300

    res.json

--- backend/src/server.js
+++ backend/src/server.js
@@ -3,7 +3,9 @@ const app = express();
 const port = 3000;
 
 app.get("/",(req,res)=>{
-    return res.send("oi");
+    return res.json({
+        message : "Hello World"
+    })
 })
 
 app.listen(port,()=>{
