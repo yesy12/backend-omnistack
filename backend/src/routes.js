81d7df4c1114208e5b02ac25508c8c13997daab7
Alison <alisonvieira.av29@gmail.com>
2019-10-02 20:03:52 -0300

    rotas em routes.js

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,4 +1,17 @@
 const express = require("express");
 const routes = express.Router;
 
+//req.query para acessar os parametros ?idade=20
+//para filtros
+
+//req.params acessar parametros da url users/1
+//para edicao,delete
+
+//req.body Acessar o corpo da requisição
+
+routes.post("/users/:id",(req,res)=>{
+    return res.json(req.body)
+})
+
+
 module.exports = routes;
--- backend/src/server.js
+++ backend/src/server.js
@@ -2,6 +2,8 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
+app.use(express.json())
+
 app.listen(port,()=>{
     console.log(`Running on port ${port} ...`)
 })
