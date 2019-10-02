3b605a9a0012d1b201b8f558c04dad3837a42330
Alison <alisonvieira.av29@gmail.com>
2019-10-02 20:05:01 -0300

    import routes.js

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const app = express();
 const port = 3000;
+const routes = require("./routes");
 
-app.use(express.json())
+app.use(express.json());
+app.use(routes);
 
 app.listen(port,()=>{
     console.log(`Running on port ${port} ...`)
