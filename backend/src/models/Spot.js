bde19a9949f1acccf1ff9dd7a44d755ac17edb24
alisonvieira.av29 <alisonvieira.av29@gmail.com>
2019-10-03 11:37:24 -0400

    alterado model Spot

--- backend/src/models/Spot.js
+++ backend/src/models/Spot.js
@@ -1,7 +1,14 @@
 const mongoose = require("mongoose");
 
 const SpotSchema = mongoose.Schema({
-	
+	thumbnail : String,
+	company : String,
+	price : Number,
+	techs : [Strings],
+	user: {
+		type : mongoose.Schema.Types.ObjectId,
+		ref: "User"
+	}
 });
 
 module.export = mongoose.model("Spot",SpotSchema)
