16efef89960b09c9798a874b3f662bfdcd239d4c
alisonvieira.av29 <alisonvieira.av29@gmail.com>
2019-10-04 18:27:06 -0400

    ops erro

--- src/controllers/SpotController.js
+++ src/controllers/SpotController.js
@@ -21,7 +21,7 @@ module.exports = {
 	async store(req,res){
 		const { filename } = req.file;
 		const { company, price, techs } = req.body;
-		const { user_id } = req.body;//req.headers
+		const { user_id } = req.headers;
 
 		const user = await User.findById(user_id)
 
